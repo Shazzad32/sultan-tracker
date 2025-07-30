2251d597ec1da2541cfda8c67a6aacff3ce04e64
Forbit <sohel@kali>
2025-07-30 17:04:39 +0600

    add facebook and whatsapp feacher

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast'
+import ContactFloatingButtons from "@/components/ContactFloatingButtons";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ContactFloatingButtons/>
         <Toaster position="right-center" reverseOrder={false} />
       </body>
     </html>
---
+++ components/ContactFloatingButtons.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import { facebook } from "../public/image-forlder/facebook.png";
+import { whatsapp } from "../public/image-forlder/whatsapp.png";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
+const ContactFloatingButtons = () => {
+  return (
+    <div className="fixed bottom-5 right-5 flex flex-col gap-3 z-50">
+      <a
+        href="https://wa.me/8801313343311"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-12 h-12 rounded-full bg-green-500 text-white shadow-lg flex items-center justify-center hover:scale-105 transition-transform"
+      >
+        <WhatsAppIcon className="w-7 h-7" />
+      </a>
+
+      <a
+        href="https://m.me/sultanvts"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-12 h-12 rounded-full bg-blue-600 shadow-lg text-white flex items-center justify-center hover:scale-105 transition-transform"
+      >
+        {/* <img src="/image-folder/facebook.png" alt="" className="w-7 h-7" /> */}
+        <FacebookRoundedIcon className="w-7 h-7" />
+      </a>
+    </div>
+  );
+};
+
+export default ContactFloatingButtons;
